680eeb3e718da48166cb7095bc8463c80038e6c6
Eduardo Aviles <eduardo.avilesj@gmail.com>
2025-09-23 17:54:55 -0600

    adding wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,33 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 16
+
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_tab_bar = true
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
+
+config.color_scheme = "Tokyo Night"
+-- my coolnight colorscheme:
+config.colors = {
+	-- foreground = "#CBE0F0",
+	-- background = "#011423",
+	cursor_bg = "#b4fa72",
+	-- cursor_border = "#47FF9C",
+	-- cursor_fg = "#011423",
+	-- selection_bg = "#033259",
+	-- selection_fg = "#CBE0F0",
+	-- ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	-- brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+-- -- and finally, return the configuration to wezterm
+return config
